a1edfbe7696f74a2c6baf44c7bcf3021f62b85f0
Gautam Jha <gauti.jha37@gmail.com>
2024-12-14 20:59:57 +0000

    cli -> cli + importable sampler

--- flux/pipeline.py
+++ flux/pipeline.py
@@ -15,48 +15,7 @@ from flux.util import configs, load_ae, load_clip, load_flow_model, load_t5, sav
 
 NSFW_THRESHOLD = 0.85
 
-@torch.inference_mode()
-def main(
-    name: str = "flux-schnell",
-    width: int = 1360,
-    height: int = 768,
-    seed: int | None = None,
-    prompt: str = "A girl sitting in a car",
-    device: str = "cuda" if torch.cuda.is_available() else "cpu",
-    num_steps: int | None = None,
-    guidance: float = 3.5,
-    offload: bool = False,
-    output_dir: str = "output",
-):
-    """
-    Sample the flux model.
-
-    Args:
-        name: Name of the model to load
-        height: height of the sample in pixels (should be a multiple of 16)
-        width: width of the sample in pixels (should be a multiple of 16)
-        seed: Set a seed for sampling
-        output_name: where to save the output image, `{idx}` will be replaced
-            by the index of the sample
-        prompt: Prompt used for sampling
-        device: Pytorch device
-        num_steps: number of sampling steps (default 4 for schnell, 50 for guidance distilled)
-        guidance: guidance value used for guidance distillation
-    """
-    nsfw_classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection", device=device)
-
-    if name not in configs:
-        available = ", ".join(configs.keys())
-        raise ValueError(f"Got unknown model name: {name}, chose from {available}")
-
-    torch_device = torch.device(device)
-    if num_steps is None:
-        num_steps = 4 if name == "flux-schnell" else 50
-
-    # allow for packing and conversion to latent space
-    height = 16 * (height // 16)
-    width = 16 * (width // 16)
-
+def get_output_filename(output_dir):
     output_name = os.path.join(output_dir, "img_{idx}.jpg")
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -67,74 +26,122 @@ def main(
             idx = max(int(fn.split("_")[-1].split(".")[0]) for fn in fns) + 1
         else:
             idx = 0
+    return output_name.format(idx=idx)
 
-    # init all components
-    t5 = load_t5(torch_device, max_length=256 if name == "flux-schnell" else 512)
-    clip = load_clip(torch_device)
-    model = load_flow_model(name, device="cpu" if offload else torch_device)
-    ae = load_ae(name, device="cpu" if offload else torch_device)
+class FluxSampler:
+    def __init__(
+        self,
+        name: str = "flux-schnell",
+        device: str = "cuda" if torch.cuda.is_available() else "cpu",
+        output_dir: str = None,
+    ):
+        self.name = name
+        self.torch_device = torch.device(device)
+        self.output_dir = output_dir
+        
+        # init all components
+        if name not in configs:
+            available = ", ".join(configs.keys())
+            raise ValueError(f"Got unknown model name: {name}, chose from {available}")
+        
+        self.t5 = load_t5(self.torch_device, max_length=256 if name == "flux-schnell" else 512)
+        self.clip = load_clip(self.torch_device)
+        self.model = load_flow_model(name, self.torch_device)
+        self.ae = load_ae(name, self.torch_device)
 
-    rng = torch.Generator(device="cpu")
+    @torch.inference_mode()
+    def __call__(
+        self,
+        prompt: str = "Indian girl sitting in an auto",
+        width: int = 1360,
+        height: int = 768,
+        seed: int | None = None,
+        num_steps: int | None = None,
+        guidance: float = 3.5
+    ):
+        """
+        Sample the flux model.
 
-    if seed is None:
-        seed = rng.seed()
-    print(f"Generating image with {seed=}:\n{prompt=}")
-    t0 = time.perf_counter()
+        Args:
+            name: Name of the model to load
+            height: height of the sample in pixels (should be a multiple of 16)
+            width: width of the sample in pixels (should be a multiple of 16)
+            seed: Set a seed for sampling
+            output_name: where to save the output image, `{idx}` will be replaced
+                by the index of the sample
+            prompt: Prompt used for sampling
+            device: Pytorch device
+            num_steps: number of sampling steps (default 4 for schnell, 50 for guidance distilled)
+            guidance: guidance value used for guidance distillation
+        """
+        # nsfw_classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection", device=device)
 
-    # prepare input
-    x = get_noise(
-        1,
-        height,
-        width,
-        device=torch_device,
-        dtype=torch.bfloat16,
-        seed=seed,
-    )
+        if num_steps is None:
+            num_steps = 4 if self.name == "flux-schnell" else 50
 
-    if offload:
-        ae = ae.cpu()
-        torch.cuda.empty_cache()
-        t5, clip = t5.to(torch_device), clip.to(torch_device)
-    inp = prepare(t5, clip, x, prompt=prompt)
-    timesteps = get_schedule(num_steps, inp["img"].shape[1], shift=(name != "flux-schnell"))
+        # allow for packing and conversion to latent space
+        height = 16 * (height // 16)
+        width = 16 * (width // 16)
 
-    # offload TEs to CPU, load model to gpu
-    if offload:
-        t5, clip = t5.cpu(), clip.cpu()
-        torch.cuda.empty_cache()
-        model = model.to(torch_device)
+        rng = torch.Generator(device="cpu")
 
-    # denoise initial noise
-    x = denoise(model, **inp, timesteps=timesteps, guidance=guidance)
+        if seed is None:
+            seed = rng.seed()
+        print(f"Generating image with {seed=}:\n{prompt=}")
+        t0 = time.perf_counter()
 
-    # offload model, load autoencoder to gpu
-    if offload:
-        model.cpu()
-        torch.cuda.empty_cache()
-        ae.decoder.to(x.device)
+        # prepare input
+        x = get_noise(
+            1,
+            height,
+            width,
+            device=self.torch_device,
+            dtype=torch.bfloat16,
+            seed=seed,
+        )
 
-    # decode latents to pixel space
-    x = unpack(x.float(), height, width)
-    with torch.autocast(device_type=torch_device.type, dtype=torch.bfloat16):
-        x = ae.decode(x)
+        inp = prepare(self.t5, self.clip, x, prompt=prompt)
+        timesteps = get_schedule(num_steps, inp["img"].shape[1], shift=(self.name != "flux-schnell"))
 
-    if torch.cuda.is_available():
-        torch.cuda.synchronize()
-    t1 = time.perf_counter()
+        # denoise initial noise
+        x = denoise(self.model, **inp, timesteps=timesteps, guidance=guidance)
 
-    fn = output_name.format(idx=idx)
-    print(f"Done in {t1 - t0:.1f}s. Saving {fn}")
+        # decode latents to pixel space
+        x = unpack(x.float(), height, width)
+        with torch.autocast(device_type=self.torch_device.type, dtype=torch.bfloat16):
+            x = self.ae.decode(x)
 
-    x = x.clamp(-1, 1)
-    x = rearrange(x[0], "c h w -> h w c")
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        t1 = time.perf_counter()
+                
+        print(f"Done in {t1 - t0:.1f}s.")
 
-    img = Image.fromarray((127.5 * (x + 1.0)).cpu().byte().numpy())
-    if output_dir:
-        img.save(fn)
-        return
-    # nsfw_score = [x["score"] for x in nsfw_classifier(img) if x["label"] == "nsfw"][0]
-    return img
+        x = x.clamp(-1, 1)
+        x = rearrange(x[0], "c h w -> h w c")
 
+        img = Image.fromarray((127.5 * (x + 1.0)).cpu().byte().numpy())
+        if self.output_dir:
+            fn = get_output_filename(self.output_dir)
+            print(f"Saving image to {fn=}")
+            img.save(fn)
+        # nsfw_score = [x["score"] for x in nsfw_classifier(img) if x["label"] == "nsfw"][0]
+        return img
+
+
+def main(
+    prompt: str = "Indian girl sitting in an auto",
+    name: str = "flux-schnell",
+    width: int = 1360,
+    height: int = 768,
+    seed: int | None = None,
+    num_steps: int | None = None,
+    guidance: float = 3.5,
+    device: str = "cuda" if torch.cuda.is_available() else "cpu",
+    output_dir: str = "output"
+):
+    flux_sampler = FluxSampler(name=name, device=device, output_dir=output_dir)
+    _ = flux_sampler(prompt, width, height, seed, num_steps, guidance)
 
 def app():
     Fire(main)
